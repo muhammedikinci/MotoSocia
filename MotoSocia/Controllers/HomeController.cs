75353c02795eb2d71a2a22ca9a57a563aa072d12
muhammedikinci <muhammedikinci@outlook.com>
2019-07-15 13:26:14 +0300

    new controller : user, edited signup and home views

--- MotoSocia/Controllers/HomeController.cs
+++ MotoSocia/Controllers/HomeController.cs
@@ -1,51 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Application;
-using Application.Commands.User;
-using Application.Models;
-using System.IO;
-using System.Text;
-using PaulMiami.AspNetCore.Mvc.Recaptcha;
 
 namespace MotoSocia.Controllers
 {
     public class HomeController : Controller
     {
-        private Persistence.MotoDBContext _context;
-        private Invoker Inv;
-
-        public HomeController(Persistence.MotoDBContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateRecaptcha]
-        public IActionResult CreateNewUser(User user)
-        {
-
-            if (ModelState.IsValid)
-            {
-                CreateNewUser CreateNewUserCommand = new CreateNewUser(_context, user);
-
-                Inv = new Invoker(CreateNewUserCommand);
-                Inv.Execute();
-
-                ViewBag.Result = "Başarılı";
-                return View("Index");
-            }
-            else
-            {
-                ViewBag.Result = "Başarısız";
-                return View("Index");
-            }
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
---
+++ MotoSocia/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Application;
+using Application.Commands.User;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using PaulMiami.AspNetCore.Mvc.Recaptcha;
+
+namespace WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private Persistence.MotoDBContext _context;
+        private Invoker Inv;
+
+        public UserController(Persistence.MotoDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateRecaptcha]
+        public IActionResult CreateNewUser(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                CreateNewUser CreateNewUserCommand = new CreateNewUser(_context, user);
+
+                Inv = new Invoker(CreateNewUserCommand);
+                Inv.Execute();
+
+                ViewBag.Result = "Kaydınız Başarıyla Yapılmıştır.";
+                return View("SignUp");
+            }
+            else
+            {
+                ViewBag.Result = "Geçersiz CAPTCHA";
+                return View("SignUp");
+            }
+        }
+    }
+}
