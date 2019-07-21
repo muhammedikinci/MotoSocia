0d31a7aa1e9ae8adc139341ad09f07c1915c5a6e
muhammedikinci <muhammedikinci@outlook.com>
2019-07-21 13:38:35 +0300

    Add command interface and invoker

---
+++ Application/Command/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Command
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
---
+++ Application/Command/Invoker.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Command
+{
+    public class Invoker<T> where T : ICommand
+    {
+        private T target;
+
+        public Invoker(T target)
+        {
+            this.target = target;
+        }
+
+        public void Invoke()
+        {
+            target.Execute();
+        }
+    }
+}
