c8668f17becfe0b8885b278100bd22b43af4334a
muhammedikinci <muhammedikinci@outlook.com>
2019-07-23 01:44:48 +0300

    Email service

---
+++ Infrastructure/Email/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Models.User;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Infrastructure.Email
+{
+    public static class EmailService
+    {
+        public static SendGridClient client;
+
+        public static async Task<Response> SendMail<TMailType>(string userEmail, string userName, string userSurname) where TMailType : MailType
+        {
+            TMailType mailType = Activator.CreateInstance<TMailType>();
+
+            var from = new EmailAddress("test@test.com", "Test test");
+            var subject = mailType.subject;
+            var to = new EmailAddress(userEmail, userName + " " + userSurname);
+            var plainTextContent = mailType.plainTextContent;
+            var htmlContent = mailType.htmlContent;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            return await client.SendEmailAsync(msg);
+        }
+    }
+}
---
+++ Infrastructure/Email/MailType.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Email
+{
+    public abstract class MailType
+    {
+        public string subject { get; set; }
+        public string plainTextContent { get; set; }
+        public string htmlContent { get; set; }
+    }
+
+    public class ConfirmMail : MailType
+    {
+        public ConfirmMail()
+        {
+            subject = "MotoSocia | Confirm Mail";
+            plainTextContent = "MotoSocia | Confirm Mail";
+            htmlContent = "<strong><button>Confirm</button></strong>";
+        }
+    }
+}
